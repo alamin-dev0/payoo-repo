262523be1422c37478ef2622f59d99025475266f
Al Amin <alamin.dev01@gmail.com>
2026-02-23 03:34:48 +0600

    added login code

--- script/login.js
+++ script/login.js
@@ -1,4 +1,25 @@
 document.getElementById('login-btn')
     .addEventListener('click', function () {
-        // console.log('login btn clicked')
+        //1- get the mobile number
+        const numberInput = document.getElementById('input-number')
+        const contactNumber = numberInput.value;
+        console.log(contactNumber)
+
+        // 2- get the pin
+        const pinInput = document.getElementById('input-pin')
+        const pin = pinInput.value
+        console.log(pin)
+        // 3- match pin and mobile number
+        if (contactNumber == '01234567890' && pin == '1234') {
+            //3-1 true ::: > alert > home page 
+            alert('login Success')
+            // window.location.replace('/home.html')
+            window.location.assign('/home.html')
+        }
+        else {
+            //3-2 true ::: > alert > return 
+            alert('login failed')
+            return;
+
+        }
     })
