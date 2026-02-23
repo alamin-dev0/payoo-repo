3a69c82ebea029a9cabb372c257c4e860e9eebb5
alamin-dev0 <alamin.dev01@gmail.com>
2026-02-23 16:09:25 +0600

    added cashout html and some js file

---
+++ script/cashout.js
@@ -0,0 +1,10 @@
+document.getElementById('cashout-btn')
+    .addEventListener('click', function () {
+        // 1. get the agent number & validate 
+        // 2. get the amount , validate , convert to number 
+        // 3. get the current balace , validate , convert to number 
+        // 4.  calculate new Balance 
+        // 5. get the pin and verify
+        // 5-1. true : show and alert > set balance 
+        // 5-2 . true : show anerror alert > return
+    })
