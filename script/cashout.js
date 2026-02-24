a03e7c437345715f2e0b89798760cf1fa4c0b9cc
Al Amin <alamin.dev01@gmail.com>
2026-02-24 23:03:11 +0600

    added some code

--- script/cashout.js
+++ script/cashout.js
@@ -1,10 +1,16 @@
 document.getElementById('cashout-btn')
     .addEventListener('click', function () {
         // 1. get the agent number & validate 
+        const cashoutNumberInput = document.getElementById('cashout-number');
+        const cashoutNumber = cashoutNumberInput.value;
+        console.log(cashoutNumber)
         // 2. get the amount , validate , convert to number 
-        // 3. get the current balace , validate , convert to number 
+        const cashoutAmountInput = document.getElementById('cashout-amount');
+        const cashoutAmount = cashoutAmountInput.value;
+        console.log(cashoutAmount)
+        // 3. get the current balance , validate , convert to number 
         // 4.  calculate new Balance 
         // 5. get the pin and verify
         // 5-1. true : show and alert > set balance 
-        // 5-2 . true : show anerror alert > return
+        // 5-2 . true : show an error alert > return
     })
